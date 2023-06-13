444c76a1353dc6e2982c910be80ad9382d6b10d8
Garrett Andrews <garrettjandrews@gmail.com>
2023-06-13 11:55:14 -0400

    through 3.6

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 
 let persons = [
     { 
@@ -24,5 +25,84 @@ let persons = [
 ]
 
 app.get("/", (req, res) => {
-    res.send('<h1>Hello world!</h1>')
+    res.send('<h1>Phonebook API</h1>')
+})
+
+app.get("/info", (req, res) => {
+    res.write(`<p>Phonebook has info for ${persons.length} people</p>`)
+    res.write(`<p>${Date()}</p>`)
+    res.end()
+})
+
+app.get("/api/persons", (req, res) => {
+    res.json(persons)
+})
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        persons = persons.filter(person => person.id !== id)
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+})
+
+// define a post method for adding a person to the phonebook
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000)
+}
+
+app.use(express.json())
+app.post("/api/persons", (req, res) => {
+    const person = req.body
+
+    // check that the proper thing was passed in
+    if (!person.name) {
+        res.status(400)
+        res.write("Error: must provide a name")
+        res.end()
+        return
+    }
+
+    if (!person.number) {
+        res.status(400)
+        res.write("Error: must provide a number")
+        res.end()
+        return
+    }
+
+    // validate that name isnt in there yet
+    const newName = person.name
+    const existingPerson = persons.find(person => person.name === newName)
+    if (existingPerson.length !== 0) {
+        res.status(400)
+        res.write("Error: name must be unique")
+        res.end()
+        return
+    }
+
+    person.id = generateId()
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
